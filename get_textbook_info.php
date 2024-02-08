fe37d312d4a82ca9763ee3f88b65a3a705ab6e24
eoverberg <eoverberg3461@gmail.com>
2024-02-07 21:15:44 -0600

    partial change yay

--- get_textbook_info.php
+++ get_textbook_info.php
@@ -5,15 +5,13 @@
 $studentData = file_get_contents("data.json");
 $textbookInfoHTML = '<h3>Textbook Information Display</h3>';
 $decodedStudentData = json_decode($studentData, true);
-foreach ($decodedStudentData as $student) {
     $textbookInfoHTML .= '<h4>' . $student['studentName'] . ' - ' . $student['courseName'] . '</h4>';
-    foreach ($student['textbooks'] as $textbook) {
+    foreach ($decodedStudentData['textbooks'] as $textbook) {
         $textbookInfoHTML .= '<p>Title: ' . $textbook['title'] . '<br>';
         $textbookInfoHTML .= 'Publisher: ' . $textbook['publisher'] . '<br>';
         $textbookInfoHTML .= 'Edition: ' . $textbook['edition'] . '<br>';
         $textbookInfoHTML .= 'Printing Date: ' . $textbook['printingDate'] . '</p>';
     }
-}
 
 // Send formatted textbook information as the response
 echo $textbookInfoHTML;
